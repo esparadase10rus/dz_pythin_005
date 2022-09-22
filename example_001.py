9356d2af7c88d1b486991ebf4cfbb223793156e9
Ilya <ackulin.ilya@yandex.ru>
2022-09-22 16:30:32 +0300

    Напишите программу, удаляющую из текста все слова, содержащие абв

--- example_001.py
+++ example_001.py
@@ -1,4 +1,11 @@
-# Задача №1:Напишите программу, удаляющую из текста все слова, содержащие ""абв"". 
+fi = open('first_file.txt', 'w')
+line = fi.write('Напишитеабв программу, удаляющую из текстаабв все слова, содержащие абв')
+fi.close()
+fi = open('first_file.txt', 'r')
+line = fi.read()
+fi.close()
+result = ' '.join(filter(lambda x: 'абв' not in x, line.split()))
+print(result)
 
-line = "Напишитеабв программу, удаляющую из текстаабв все слова, содержащие абв"
-print( " ".join(filter(lambda x: "абв" not in x, line.split())))
+with open('file.txt', 'w', encoding='utf-8') as f:
+    f.write(result)
